d77382a0f20e8518437bf288fe872c5de2a04592
MDTravis <59547361+MDTravisYT@users.noreply.github.com>
2024-06-22 07:36:42 -0400

    bullet collision is now functional

--- build.bat
+++ build.bat
@@ -1,3 +1,7 @@
+echo %date% %time% > time.txt
+
 IF NOT EXIST OUT MKDIR OUT
 asm68k /k /p /o ae-,c+ SRC/_MAIN.ASM, OUT/UT.BIN >OUT/UT.LOG, OUT/UT.SYM, OUT/UT.LST
 convsym OUT/UT.SYM OUT/UT.BIN -a
+
+del time.txt
